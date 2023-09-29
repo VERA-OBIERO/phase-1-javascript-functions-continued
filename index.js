1fb0edc26c91ae54fb432a4819c5d485e814c992
VERA-OBIERO <vera.obiero@gmail.com>
2023-09-29 12:57:55 +0300

    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function (duty = "go to the office"){
+    return `This Monday, I will ${duty}.`;
+}
+mondayWork();
+mondayWork("work from home");
+
+function wrapAdjective(symbol){
+    return function(adjective){
+        return `You are ${symbol}${adjective}${symbol}!`;
+    };
+}
+const asterisk = wrapAdjective('*');
+const doublePipe = wrapAdjective('||');
